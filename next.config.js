7b50dfd5209f830058a14d301efdea8b63adaddb
Ershad Taymourtash <ershad.sh@gmail.com>
2025-03-26 11:47:11 +0100

    Update next.config.js for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+  output: 'export',           
+  basePath: '/Final-Website', 
+  assetPrefix: '/Final-Website/' 
 };
 module.exports = nextConfig;
